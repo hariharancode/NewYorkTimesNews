e0f4f6126f78ce95d5380517dff5c5f7e552fa47
Hari Haran <hariharancode@gmail.com>
2018-07-14 18:05:09 +0530

    1.Added Dependencies in grade 2.Chaged .gitignore 3.Added ApiClientConfig to handle url building and parameters

---
+++ app/src/main/java/hariharan/com/nyorktimes/Network/ApiClientConfig.java
@@ -0,0 +1,34 @@
+package hariharan.com.nyorktimes.Network;
+
+import com.google.gson.GsonBuilder;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by hariharan on 14/07/18.
+ */
+
+public class ApiClientConfig {
+
+    private static Retrofit retrofit = null;
+    private static String BASEURL = "http://api.nytimes.com/svc/";
+
+    public static Retrofit getClient() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASEURL)
+                .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create()))
+                .client(client)
+                .build();
+
+        return retrofit;
+    }
+}
